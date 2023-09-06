5c4a15ed25564f695a706c73ca6ef8a719377fac
Michal Sobiech <michal.sobiech50@gmail.com>
2023-09-06 23:54:17 +0200

    chore: add colors to tw cfg

--- client/tailwind.config.js
+++ client/tailwind.config.js
@@ -5,11 +5,24 @@ export default {
   theme: {
     extend: {
       colors: {
-        fb: {
-          blue: "hsl(213 89% 52%)",
-          "blue-dark": "hsl(214 82% 49%)",
-          green: "#42b72a",
-          "green-dark": "#36a420",
+        "fb-blue": {
+          DEFAULT: "hsl(213 89% 52%)",
+          dark: "hsl(214 82% 49%)",
+        },
+        "fb-green": {
+          DEFAULT: "#42b72a",
+          dark: "#36a420",
+        },
+        "fb-gray": {
+          100: "hsla(0, 0%, 83%, 1)",
+          200: "hsla(0, 0%, 65%, 1)",
+          300: "hsla(0, 0%, 44%, 1)",
+          400: "hsla(0, 0%, 31%, 1)",
+          500: "hsla(0, 0%, 25%, 1)",
+          600: "hsla(0, 0%, 20%, 1)",
+          700: "hsla(0, 0%, 15%, 1)",
+          800: "hsla(0, 0%, 10%, 1)",
+          900: "hsla(0, 0%, 7%, 1)",
         },
       },
       screens: {
