e7174ae9655c565210fd2cdf2d6a68bae54ee85d
Michal Sobiech <michal.sobiech50@gmail.com>
2023-11-11 00:21:08 +0100

    fix(server): make errors respond with desc in the same property
    Make the error description return in the same message property
    regardless of the cause of the error

--- server/middlewares/errorHandler.js
+++ server/middlewares/errorHandler.js
@@ -17,7 +17,7 @@ const errorHandler = (error, req, res, next) => {
       .send({ message: "Internal Server Error" });
   }
 
-  return res.status(error.statusCode).send({ error: error.message });
+  return res.status(error.statusCode).send({ message: error.message });
 };
 
 export default errorHandler;
